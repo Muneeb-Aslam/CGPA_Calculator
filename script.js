615ece791838d789cf81a5a94d97db71fa654bad
Muneeb <muneeb.official.686332@hotmail.com>
2022-12-18 20:35:47 +0500

    Home Page

---
+++ script.js
@@ -0,0 +1,10 @@
+let cgpa = document.getElementById("cgpa");
+let gpa = document.getElementById("gpa");
+
+gpa.addEventListener('click',()=>{
+    location.href("gpa.html");
+});
+
+cgpa.addEventListener('click',()=>{
+    location.href("cgpa.html");
+});
