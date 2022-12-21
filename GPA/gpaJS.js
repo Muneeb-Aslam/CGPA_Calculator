58f059f2a4a46d2251a5ee8523f2d5961a83f76b
Muneeb <muneeb.official.686332@hotmail.com>
2022-12-21 20:39:39 +0500

    GPA Done

--- GPA/gpaJS.js
+++ GPA/gpaJS.js
@@ -6,40 +6,82 @@ let SemesterNo = document.getElementById("SemesterNo");
 let subjects = document.getElementById("totalsubjects");
 
 function setAttributes(el, attrs) {
-    for(var key in attrs) {
-      el.setAttribute(key, attrs[key]);
-    }
+  for (var key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
+};
+
+let gradebutton = document.createElement("button");
+
+enterbutton.addEventListener("click", () => {
+
+  
+  gradebutton.innerText = "Calculate";
+  gradebutton.setAttribute("class", "Enterbutton")
+
+  SemesterNo.innerText = SemesterNo.textContent + " " + semesterinput.value;
+  subjects.innerText = subjects.textContent + " " + subjectsinput.value;
+  let intsubjects = parseInt(subjectsinput.value);
+  
+  
+  for (let i = 0; i < intsubjects; i++) {
+    let div = document.createElement("div")
+    let subjectname = document.createElement("input");
+    let credithour = document.createElement("input");
+    let grade = document.createElement("input");
+
+    setAttributes(div, { "class": "gradesinput" })
+    setAttributes(subjectname, { "class": "input", "placeholder": "Enter Subject Name:" })
+    setAttributes(credithour, { "class": "input", "placeholder": "Enter Credits Hour:", "id": `Hour${i}` })
+    setAttributes(grade, { "class": "input", "placeholder": "Enter Grade:", "id": `grade${i}` })
+
+    subjectsdiv.appendChild(div)
+    div.appendChild(subjectname)
+    div.appendChild(credithour)
+    div.appendChild(grade)
+
   }
 
-enterbutton.addEventListener("click",()=>{
+  subjectsdiv.appendChild(gradebutton)
 
-    let gradebutton = document.createElement("button");
-    gradebutton.innerText="Calculate";
-    gradebutton.setAttribute("class","Enterbutton")
 
-    SemesterNo.innerText=SemesterNo.textContent+" "+semesterinput.value;
-    subjects.innerText=subjects.textContent+" "+subjectsinput.value;
-    let intsubjects = parseInt(subjectsinput.value);
-    console.log(intsubjects);
-    for(let i=0;i<intsubjects;i++){
-        let div = document.createElement("div")
-        let subjectname = document.createElement("input");
-        let credithour = document.createElement("input");
-        let grade = document.createElement("input");
-        
-        setAttributes(div,{"class":"gradesinput"})
-        setAttributes(subjectname,{"class":"input","placeholder":"Enter Subject Name:"})
-        setAttributes(credithour,{"class":"input","placeholder":"Enter Credits Hour:"})
-        setAttributes(grade,{"class":"input","placeholder":"Enter Grade:"})
-
-        subjectsdiv.appendChild(div)
-        div.appendChild(subjectname)
-        div.appendChild(credithour)
-        div.appendChild(grade)
-
-    }
-
-    subjectsdiv.appendChild(gradebutton)
-
-    
 });
+
+let gpacalculated = document.getElementById("gpacalculated")
+
+gradebutton.addEventListener("click", () => {
+  const result = CalculateGPA(parseInt(subjectsinput.value));
+  gpacalculated.innerText=`Your GPA is: ${result}`;
+});
+
+
+function CalculateGPA(subjects) {
+  let result = 0.0;
+  let creditHourSum = 0;
+  for (let i = 0; i < subjects; i++) {
+   let grade = document.getElementById(`grade${i}`).value;
+    let creditHour = parseInt(document.getElementById(`Hour${i}`).value);
+
+    creditHourSum = creditHourSum + creditHour
+    
+    if (grade === "A")
+      result += (4 * creditHour);
+    else if (grade === "B+")
+      result += (3.5 * creditHour);
+    else if (grade === "B")
+      result += (3 * creditHour);
+    else if (grade === "C+")
+      result += (2.5 * creditHour);
+    else if (grade === "C")
+      result += (2 * creditHour);
+    else if (grade === "D+")
+      result += (1.5 * creditHour);
+    else if (grade === "D")
+      result += (1 * creditHour);
+    else if (grade === "F")
+      result += (0 * creditHour);
+  }
+
+  
+  return (result / creditHourSum).toFixed(2);
+}
